997198da14797e7036cc945f140aeee238972167
Amruth Pillai <im.amruth@gmail.com>
2019-10-11 20:46:33 +0530

    Updates

---
+++ precache-manifest.bef607507f26f1308805575560bf7c93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e87fec982ba9b2fe8862281e96df1f91",
+    "url": "/ReelDB-React/index.html"
+  },
+  {
+    "revision": "6ee35208bc6564ee37de",
+    "url": "/ReelDB-React/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "1d61ac54e5e6e9c46b55",
+    "url": "/ReelDB-React/static/css/main.c994fdef.chunk.css"
+  },
+  {
+    "revision": "6ee35208bc6564ee37de",
+    "url": "/ReelDB-React/static/js/2.6a60d6c9.chunk.js"
+  },
+  {
+    "revision": "1d61ac54e5e6e9c46b55",
+    "url": "/ReelDB-React/static/js/main.f59fc8ac.chunk.js"
+  },
+  {
+    "revision": "cefaa976a704550d4188",
+    "url": "/ReelDB-React/static/js/runtime-main.2579cb31.js"
+  }
+]);
